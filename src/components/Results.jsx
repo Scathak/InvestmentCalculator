94c30e0a177fcbd239c5007fc0e82b22ad31b604
Scathak <scathakocridera@gmail.com>
2024-11-24 23:53:28 +0300

    Colors changed. The code cleaned.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,6 @@
 import { calculateInvestmentResults, formatter } from '../util/investment.js';
 export default function Results({input}){
-    //console.log(input);
     const resultsData = calculateInvestmentResults(input);
-    //console.log(resultsData);
     const initialInvestment = 
       resultsData[0].valueEndOfYear -
       resultsData[0].interest - 
